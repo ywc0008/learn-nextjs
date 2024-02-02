df5860701ed3629ceb5a570525fa4d21331799c1
wzz <ywc0008@naver.com>
2024-02-02 18:08:13 +0900

    API_URL from page -> constants

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/movie-info.module.css";
-import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/constants";
 
 export async function getMovie(id: string) {
   console.log(`Fetching movies: ${Date.now()}`);
