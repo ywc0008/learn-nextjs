1c2a3d8629f697cc1f731d8e0f3d9f9c0a7d0f22
wzz <ywc0008@naver.com>
2024-02-02 17:42:40 +0900

    second commit

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,14 +1,26 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-video.module.css";
 
 async function getVideos(id: string) {
   console.log(`Fetching video: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("something broke...");
-  //   const response = await fetch(`${API_URL}/${id}/videos`);
-  //   return response.json();
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
 
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
